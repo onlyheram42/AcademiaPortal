b093bfff05e3592695edd80b08dd36c362e7a800
Sriram <gh.sriram@solera.com>
2024-07-01 21:21:23 +0530

    initial setup - angular and spring

---
+++ backend/AcademiaPortal/src/main/java/com/onlyh/academiaportal/AcademiaportalApplication.java
@@ -0,0 +1,13 @@
+package com.onlyh.academiaportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaportalApplication.class, args);
+	}
+
+}
---
+++ backend/AcademiaPortal/src/test/java/com/onlyh/academiaportal/AcademiaportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyh.academiaportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
